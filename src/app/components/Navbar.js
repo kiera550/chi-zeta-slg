6aa3e88bed9fea6f1de37c22d302b6eca63b3a90
kiera550 <kapardo10@gmail.com>
2025-07-14 18:53:15 -0400

    Changing color of navbar and fixed link to homepage

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,9 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import './Navbar.css';
 
 export default function MyNavbar() {
     return (
-        <nav class = "navbar navbar-expand-lg bg-body-tertiary">
+        <nav class = "navbar navbar-expand-lg navbar-custom">
             <div class = "container-fluid">
                 <a class = "navbar-brand" href = "#">Chi Zeta</a>
                 <button class = "navbar-toggler" type = "button" data-toggle = "collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -12,7 +12,7 @@ export default function MyNavbar() {
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item active">
-                            <a class="nav-link" href="#">Home</a>
+                            <a class="nav-link" href="/">Home</a>
                         </li>
                         <li class="nav-item active">
                             <a class="nav-link" href="/dashboard/values">Our Values</a>
--- src/app/dashboard/ourMembers/page.js
+++ src/app/dashboard/ourMembers/page.js
@@ -1,7 +1,11 @@
 'use client';
 
+
 export default function Members(){
     return(
-        <h1>Members</h1>
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+              <h1>Members here</h1>
+              
+         </div>
     )
 }

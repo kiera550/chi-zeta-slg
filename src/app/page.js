29c6ddf89a4319c77b47c812b2a355670f1233ee
kiera550 <kapardo10@gmail.com>
2025-07-14 19:25:39 -0400

    Home page containers, error: Image will not load

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,19 @@ import MyNavbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Home</h1>
-
+    
+    <div className = "container text-center margin">
+      <div className="row">
+        <div className="col-4">
+          <h1>Chi Zeta at Case Western Reserve</h1>
+          <h3>Sigma Lambda Gamma National Sorority Inc.</h3>
+          <p>Greetings from the Chi Zeta Chapter of Sigma Lambda Gamma! </p>
+        </div>
+        <div className="col-8">
+          <Image src="/images/gabiandkiera.jpeg" alt="testing" width={200} height={200}/>
+        </div>
+      </div>
     </div>
+
   );
 }

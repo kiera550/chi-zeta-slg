416f6f83ebcd44361f08545b7546f7bb39a32811
kiera550 <kapardo10@gmail.com>
2025-07-16 00:03:11 -0400

    Working on layout for homepage

--- src/app/page.js
+++ src/app/page.js
@@ -15,6 +15,30 @@ export default function Home() {
           Image to go here
         </div>
       </div>
+      <div className="row mt-4">
+        <div>
+          <h1>Culture is Pride, Pride is Success</h1>
+        </div>
+      </div>
+      <div className="row mt-4">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+      <div className="row mt-4">
+        <div className="text-start">
+          <h3>Interested in joining?</h3>
+        </div>
+      </div>
+      <div className="row mt-4">
+        <div className="col-sm">
+          <h3>Sign up for a coffee chat with a sibling!</h3>
+        </div>
+        <div className="col-sm">
+          <h3>Fill out our interest form</h3>
+        </div>
+        <div className="col-sm">
+          <p>Image here</p>
+        </div>
+      </div>
     </div>
 
   );

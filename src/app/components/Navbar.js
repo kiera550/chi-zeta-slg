93aab6d79365997edb06ea6ae312071f84d29a0a
Gabriela Hernandez <gabrielajhernandez555@gmail.com>
2025-12-18 23:04:48 -0600

    Added QR codes

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -3,28 +3,28 @@ import './Navbar.css';
 
 export default function MyNavbar() {
     return (
-        <nav class = "navbar navbar-expand-lg navbar-custom">
-            <div class = "container-fluid">
-                <a class = "navbar-brand" href = "#">Chi Zeta</a>
-                <button class = "navbar-toggler" type = "button" data-toggle = "collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+        <nav className = "navbar navbar-expand-lg navbar-custom">
+            <div className = "container-fluid">
+                <a className = "navbar-brand" href = "#">Chi Zeta</a>
+                <button className = "navbar-toggler" type = "button" data-toggle = "collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="/">Home</a>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="/">Home</a>
                         </li>
-                        <li class="nav-item active">
-                            <a class="nav-link" href="/dashboard/values">Our Values</a>
+                        <li className="nav-item active">
+                            <a className="nav-link" href="/dashboard/values">Our Values</a>
                         </li>
-                        <li class="nav-item active">
-                            <a class="nav-link" href="/dashboard/ourEvents">Events</a>
+                        <li className="nav-item active">
+                            <a className="nav-link" href="/dashboard/ourEvents">Events</a>
                         </li>
-                        <li class="nav-item active">
-                            <a class="nav-link" href="/dashboard/ourMembers">Members</a>
+                        <li className="nav-item active">
+                            <a className="nav-link" href="/dashboard/ourMembers">Members</a>
                         </li>
-                        <li class="nav-item active">
-                            <a class="nav-link" href="/dashboard/donate">Donate</a>
+                        <li className="nav-item active">
+                            <a className="nav-link" href="/dashboard/donate">Donate</a>
                         </li>
                     </ul>
                 </div>
--- src/app/dashboard/donate/page.js
+++ src/app/dashboard/donate/page.js
@@ -1,8 +1,29 @@
 'use client';
 import '../../globals.css';
+import Image from "next/image";
 
 export default function Donate(){
     return(
-        <h1>Donate to us</h1>
+        <div className="container">
+        <div className="row mt-4">
+        <div className="text-center">
+            <h1>Support Our Chapter!</h1>
+        </div>
+        </div>
+            <div className="row text-center">
+                <div className="col-6">
+                    <h2>Venmo</h2>
+                    <Image src="/images/slg_venmo.png" alt="Venmo QR Code"width={370} height={370}/>
+                    <a href="https://venmo.com/u/cwru_gammas" target="_blank"><h4>@cwru_gammas</h4></a>
+                </div>
+                <div className="col-6">
+                <h2>Zelle</h2>
+                <Image src="/images/slg_zelle.png" alt="Zelle QR Code"width={370} height={370}/>
+                <h4>gjh68@case.edu</h4>
+                </div>
+            </div>            
+        </div>
+        
+        
     )
 }
